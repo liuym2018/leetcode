5705f48dde9f79d33a8eeae9ee874c0ff16922ae
yongming <36879574+liuym2018@users.noreply.github.com>
2021-06-15 18:28:01 -0500

    Time: 76 ms (99.64%), Space: 41.4 MB (79.89%) - LeetHub

---
+++ maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,35 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    //if root is undefined, depth is 0
+    if(!root) return 0
+    // variable to store the maximum levels
+    let max = 0
+    //helper function to traverse the tree
+    //recursively increment the levels by one
+    const dfs = (node, levels) => {
+       //compare levels and max to pass the maximum levels
+        if(levels > max) max = levels
+       //traverse all children of the current node
+        for(let child of node.children){
+           //increment the levels by one
+            dfs(child, levels + 1);
+        }
+    }
+    //when root is not null, the tree has at least one level,
+    //so we pass down 1
+    dfs(root, 1)
+  
+   //return the maximum levels
+    return max
+};
