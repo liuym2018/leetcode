e06d057e31dadb8e2013be2e69447a32a2fbe1a4
yongming <36879574+liuym2018@users.noreply.github.com>
2021-06-15 13:31:15 -0500

    Time: 88 ms (91.68%), Space: 41.9 MB (9.47%) - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums == null) {
+        return 0;
+    }
+    let i = 0;
+    let j = 1;
+    while (j < nums.length) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+            j++;
+        } else {
+            j++
+        }
+    }
+    return i + 1;
+};
