fb1cc972a630d29ba450434313fff42b679c6794
yongming <36879574+liuym2018@users.noreply.github.com>
2021-06-14 22:05:31 -0500

    Time: 76 ms (66.82%), Space: 38.4 MB (50.55%) - LeetHub

---
+++ to-lower-case/to-lower-case.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    // let ans = ""
+    // for (let c of s) {
+    //     let n = c.charCodeAt()
+    //     console.log('c: ', c.charCodeAt())
+    //     ans += n > 64 && n < 91 ? String.fromCharCode(n + 32) : c 
+    // }
+    // return ans
+    return s.toLowerCase();
+};
